5896f75f1219252552890e5c807146da6827d35d
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2020-04-06 09:09:29 +0100

    Refactored interfaces, changed shape parameters to coefficient of variation, fixed cdr

--- outbreak_modelling.py
+++ outbreak_modelling.py
@@ -23,14 +23,79 @@ class SEIR(namedtuple('SEIR', ['S', 'E', 'I', 'R'])):
 
 class SEIRModel:
     def __init__(self,
-                 R_0=3.3,
-                 T_inc=4.6,
-                 T_inf=1.4):
+                 initial_state,
+                 R_0=None,
+                 T_inc=None,
+                 T_inf=None,
+                 early_growth_rate=None,
+                 mean_generation_time=None,
+                 lat_fraction=None):
+        #TODO: input validation
+        alt_parm = R_0 is None or T_inc is None or T_inf is None
+        std_parm = (early_growth_rate is None or mean_generation_time is None or
+                    lat_fraction is None)
+        if not (alt_parm or std_parm):
+            raise ValueError('You have to either provide all of R_0, T_inc '
+                             'and T_inf or all of early_growth_rate, '
+                             'mean_generation_time and lat_frac')
+        if alt_parm:
+            params = SEIRModel.calibrate_parameters(early_growth_rate,
+                                                    mean_generation_time,
+                                                    lat_fraction)
+            R_0, T_inc, T_inf = params['R_0'], params['T_inc'], params['T_inf']
         self.R_0 = R_0 if callable(R_0) else (lambda _: R_0)
         self.T_inc = T_inc
         self.T_inf = T_inf
-        
-    def __call__(self, t, y):
+        self.initial_state = initial_state
+        self.reset_cache()
+
+    @property
+    def R_0(self):
+        return self._R_0
+
+    @R_0.setter
+    def R_0(self, value):
+        self._R_0 = value if callable(value) else (lambda _: value)
+        self.reset_cache()
+
+    @property
+    def T_inc(self):
+        return self._T_inc
+
+    @T_inc.setter
+    def T_inc(self, value):
+        self._T_inc = value
+        self.reset_cache()
+
+    @property
+    def T_inf(self):
+        return self._T_inf
+
+    @T_inf.setter
+    def T_inf(self, value):
+        self._T_inf = value
+        self.reset_cache()
+
+    def reset_cache(self):
+        self._path = None
+
+    @staticmethod
+    def calibrate_parameters(early_growth_rate,
+                             mean_generation_time,
+                             lat_fraction):
+        """
+        Calculate SEIR parameters R_0, T_inc and T_inf calibrated to
+        the specified initial daily case/death growth rate, the infection
+        mean generation time, and the fraction of this time that the
+        infection is assumed to be latent (i.e. non-infectious, E stage).
+        """
+        T_inc = lat_fraction * mean_generation_time
+        T_inf = mean_generation_time - T_inc
+        pos_eigenvalue = np.log(1+early_growth_rate)
+        R_0 = (1 + pos_eigenvalue * T_inc) * (1 + pos_eigenvalue * T_inf)
+        return {'R_0':R_0, 'T_inc':T_inc, 'T_inf':T_inf}
+         
+    def _ydot(self, t, y):
         y = SEIR.from_a(y)
         N = y.S + y.E + y.I + y.R
         beta = self.R_0(t) / (N * self.T_inf)
@@ -39,113 +104,141 @@ class SEIRModel:
         Id = y.E / self.T_inc - y.I / self.T_inf
         Rd = y.I / self.T_inf        
         return SEIR.make_state(S=Sd, E=Ed, I=Id, R=Rd).to_a()
+
+    def predict(self, sim_days, T_start=0):
+        if (self._path is not None and len(self._path)>sim_days):
+            # Use cached result if possible
+            result = self._path.iloc[:sim_days+1]
+            result.index = result.index - result.index[0] + T_start
+            return result
+        else:
+            sim_days = int(sim_days)
+            t_eval = np.linspace(T_start, T_start+sim_days, sim_days+1)
+            ivp = solve_ivp(self._ydot, (0, sim_days),
+                            self.initial_state.to_a(), t_eval=t_eval)
+            if ivp.status != 0:
+                print(ivp.message)
+            self._path = pd.DataFrame(ivp.y.T,
+                                      index=ivp.t,
+                                      columns=self.initial_state.state_labels)
+            return self._path.copy()
     
-class EmissionModel:
-    def __init__(self,
-                 f_cdr=0.044,
-                 f_cfr = 0.009,
-                 n_detect = 4.,
-                 T_detect = 11.,
-                 n_resolve=9,
-                 T_resolve=8.,
-                 n_death=4,
-                 T_death=14.):
+class SEIRObsModel(SEIRModel):
+    def __init__(self, f_cdr, f_cfr, cv_detect, T_detect, cv_resolve, T_resolve,
+                 cv_death, T_death, start_date=0, **SEIR_params):
+        #TODO: input validation
+        n_detect, n_resolve, n_death = [cv**(-2) for cv in
+                                        (cv_detect, cv_resolve, cv_death)]
+        self._fullpath = None
         self.p_symptoms = f_cdr * dg_weights(n_detect,
                                               T_detect/n_detect,
                                               int(T_detect*30))
-        self.p_resolve = (1-f_cfr/f_cdr) * dg_weights(n_resolve,
-                                                  T_resolve/n_resolve,
-                                                  int(T_resolve*30))
-        self.p_death = (f_cfr / f_cdr) * dg_weights(n_death,
-                                                    T_death/n_death,
-                                                    int(T_death*30))
+        self.p_resolve = (1-f_cfr) * dg_weights(n_resolve,
+                                                T_resolve/n_resolve,
+                                                int(T_resolve*30))
+        self.p_death = f_cfr * dg_weights(n_death,
+                                          T_death/n_death,
+                                          int(T_death*30))
+        self.start_date = start_date
+        super().__init__(**SEIR_params)
+
+    @property
+    def start_date(self):
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str):
+            start_date = pd.to_datetime(start_date)
+        self._start_date = start_date
+        self._offset = (pd.Timedelta(1, 'D')
+                        if isinstance(self._start_date, pd.Timestamp)
+                        else 1.)
+
+    def reset_cache(self):
+        self._fullpath = None
+        super().reset_cache()
         
-    def add_observables(self, sim):
-        sim['All exposed'] = sim.E + sim.I + sim.R
-        sim['Daily exposed'] = sim['All exposed'].diff()
-        sim['Daily exposed'].iloc[0] = 0.
-        sim['Daily cases'] = dp_convolve(sim['Daily exposed'],
-                                            self.p_symptoms)
-        sim['Daily recoveries'] = dp_convolve(sim['Daily cases'],
-                                              self.p_resolve)
-        sim['Daily deaths'] = dp_convolve(sim['Daily cases'],
-                                          self.p_death)
-        sim['All cases'] = sim['Daily cases'].cumsum()
-        sim['All recoveries'] = sim['Daily recoveries'].cumsum()
-        sim['All deaths'] = sim['Daily deaths'].cumsum()
-        sim['Active cases'] = (sim['All cases'] -
-                               sim['All recoveries'] -
-                               sim['All deaths'])
-        return sim
-        
-def calibrate_transmission_model(early_growth_rate,
-                                 mean_generation_time,
-                                 lat_fraction):
-    """
-    Returns R_0, T_inc and T_inf for a SEIR model calibrated to
-    the specified initial daily case/death growth rate, the infection
-    mean generation time, and the fraction of this time that the
-    infection is assumed to be latent (i.e. non-infectious, E stage).
-    """
-    T_inc = lat_fraction * mean_generation_time
-    T_inf = mean_generation_time - T_inc
-    pos_eigenvalue = np.log(1+early_growth_rate)
-    R_0 = (1 + pos_eigenvalue * T_inc) * (1 + pos_eigenvalue * T_inf)
-    return {'R_0':R_0, 'T_inc': T_inc, 'T_inf': T_inf}
+    def score(self, day_zero, cases, recovered, deaths, weights=None,
+              predictions=None):
+        raise NotImplementedError()
+
+    def fit(self, cases, recovered, deaths, sim_days, obs_threshold=10,
+            weights=None):
+        """
+        Fit day 0 of the simulation to observed cases, recovered and 
+        deaths. Weights is a 3-tuple specifying the relative weights to
+        place on case data, recovery data and death data respectively when
+        calculating the score.
+        """
+        if self._fullpath is None:
+            self.predict(sim_days)
+            
+        if weights is None:
+            weights = [1/3, 1/3, 1/3]
+        w_c, w_r, w_d = weights
+
+        cases, recovered, deaths = [s.dropna()
+                                     for s in (cases, recovered, deaths)]
+
+        total_weight = (len(cases) * w_c + len(recovered) * w_r +
+                        len(deaths) * w_d)
+
+        def error_ts(days_shift, s_obs, s_pred):
+            if len(s_obs)==0:
+                return 0.
+            obs_days = (s_obs.index - self.start_date) / self._offset
+            s_pred = np.interp(obs_days + days_shift, s_pred.index,
+                               s_pred.values)
+            return np.sum(np.abs((np.log((obs_threshold+s_obs.values)/
+                                         (obs_threshold+s_pred)))))
+            
+        def score(days_shift):
+            result = (w_c * error_ts(days_shift, cases,
+                                   self._fullpath['All cases']) +
+                    w_r * error_ts(days_shift, recovered,
+                                   self._fullpath['All recovered']) +
+                    w_d * error_ts(days_shift, deaths,
+                                   self._fullpath['All deaths']))
+            return result
+
+        opt = minimize_scalar(score)
+        if not opt.success:
+            print(opt)
+            raise RuntimeError('Optimiser failed')
+        opt_shift = round(opt.x)
+        self.start_date -= opt_shift * self._offset
+        return self
     
-def run_outbreak(transmission_model,
-                 emission_model,
-                 sim_time_days, initial_state):
-    sim_time_days = int(sim_time_days)
-    t_eval = np.linspace(0, sim_time_days, sim_time_days+1)
-    ivp = solve_ivp(transmission_model, (0, sim_time_days),
-                    initial_state.to_a(), t_eval=t_eval)
-    if ivp.status != 0:
-        print(ivp.message)
-    result = pd.DataFrame(ivp.y.T,
-                          index=ivp.t,
-                          columns=initial_state.state_labels)
-    result = emission_model.add_observables(result)
-    return result
+    def predict(self, sim_days):
+        if self._fullpath is not None and len(self._fullpath)>sim_days:
+            sim =  self._fullpath.iloc[:sim_days+1]
+        else:
+            sim = super().predict(sim_days)
+            sim['All exposed'] = sim.E + sim.I + sim.R
+            sim['Daily exposed'] = sim['All exposed'].diff()
+            sim['Daily exposed'].iloc[0] = 0.
+            sim['Daily cases'] = dp_convolve(sim['Daily exposed'],
+                                             self.p_symptoms)
+            sim['Daily recovered'] = dp_convolve(sim['Daily cases'],
+                                                  self.p_resolve)
+            sim['Daily deaths'] = dp_convolve(sim['Daily cases'],
+                                              self.p_death)
+            sim['All cases'] = sim['Daily cases'].cumsum()
+            sim['All recovered'] = sim['Daily recovered'].cumsum()
+            sim['All deaths'] = sim['Daily deaths'].cumsum()
+            sim['Active cases'] = (sim['All cases'] -
+                                   sim['All recovered'] -
+                                   sim['All deaths'])
+            self._fullpath = sim
+        result = sim.copy()
+        if isinstance(self.start_date, pd.Timestamp):
+            result.index = self.start_date + pd.TimedeltaIndex(sim.index, 'D')
+        else:
+            result.index = self.start_date + sim.index.values
+        result.index.name = 'Date'
+        return result
 
-def calibrate_timing_to_cases(sim, confirmed_cases):
-    """
-    Assumes confirmed_cases is a date-indexed Series. Currently assumes
-    confirmed cases.
-    """
-    ref_date = confirmed_cases.index[0]
-    confirmed_cases = confirmed_cases.copy()
-    confirmed_cases.index = (confirmed_cases.index -
-                             confirmed_cases.index[0]).to_series().dt.days
-    def mape(shift):
-        C = np.interp(confirmed_cases.index+shift, sim.index,
-                      sim['All cases'].values)
-        return (C / confirmed_cases - 1).abs().mean()
-    opt = minimize_scalar(mape)
-    if not opt.success:
-        print(opt)
-        raise RuntimeError('Optimiser failed')
-    opt_shift = round(opt.x)
-    result = sim.copy()
-    result.index = ref_date + pd.to_timedelta(sim.index - opt_shift, unit='D')
-    result.index.name=('Date')
-    return result
-
-def identify_date(sim, confirmed_cases, target_date='2020/03/14'):
-    """
-    Assumes confirmed_cases is a date-indexed Series. Currently assumes
-    confirmed cases.
-    """
-    target_date = pd.to_datetime(target_date)
-    target_value = confirmed_cases[target_date]
-    try:
-        date_offset = sim.index[sim['All cases']>target_value][0]
-    except IndexError: 
-        raise ValueError("Cannot find suitable date offset")
-    result = sim.copy()
-    result.index = target_date + pd.to_timedelta(sim.index - date_offset,
-                                                 unit='D')
-    return result
 
 def dg_weights(n, mean, n_days):
     """ Discretise a gamma distribution over n_days timesteps
--- plotutils.py
+++ plotutils.py
@@ -5,6 +5,7 @@ import matplotlib.ticker as ticker
 import matplotlib.dates as mdates
 import seaborn as sns
 from ipywidgets import interactive, FloatSlider, fixed, Text, BoundedIntText
+from ipywidgets import Layout, Dropdown
 from outbreak_modelling import *
 
 def sims_to_longform(sims):
@@ -30,12 +31,13 @@ def plot_simulations(sims, observations, ax=None):
     sns.lineplot(data=plot_data,
                  x='Date', y='Value', hue='PROJECTION',
                  style='SIMULATION NAME',
-                 hue_order=['All cases', 'All deaths', 'Daily cases',
-                            'Daily deaths'],
+                 hue_order=['All cases', 'All deaths',
+                            'Daily cases', 'Daily deaths'],
                  dashes = ['', (2, 4)])
     ax.plot([], [], ' ', label='OBSERVATIONS')
     ax.set_prop_cycle(None)
-    observations[['confirmed', 'deaths']].plot(ax=ax, marker='x', ls='')
+    observations[['confirmed', 'deaths']].plot(ax=ax,
+                                               marker='x', ls='')
     ax.set_yscale('log')
     ax.set_ylim(1, None)
     ax.yaxis.set_major_locator(ticker.LogLocator(10., (1.,), 15, 15))
@@ -54,20 +56,25 @@ def explore_simulation(initial_growth_rate,
 		       latent_fraction,
 		       f_cdr, f_cfr,
 		       T_detect, T_resolve, T_death,
-		       n_detect, n_resolve, n_death,
+		       cv_detect, cv_resolve, cv_death,
 		       R_0_lockdown,
                        lockdown_release_date,
                        lockdown_release_timeframe_weeks,
                        sim_time_weeks,
+                       weights,
                        observations):
     initial_growth_rate /= 100
     f_cdr /= 100
     f_cfr /= 100
+    cv_detect /= 100
+    cv_resolve /= 100
+    cv_death /= 100
     
     try:
         lockdown_release_date = pd.to_datetime(lockdown_release_date)
-    except TypeError:
-        print('Error understanding lockdown release date')
+    except (TypeError, ValueError) as e:
+        print('Error understanding lockdown release date:\n')
+        print(e)
         return
     lockdown_release_end = (lockdown_release_date +
                             pd.to_timedelta(7*lockdown_release_timeframe_weeks,
@@ -75,32 +82,35 @@ def explore_simulation(initial_growth_rate,
     if lockdown_release_date < pd.to_datetime('20/04/01'):
         print('Lockdown cannot be released before April 2020')
         return
-        
-    transmission_parameters = calibrate_transmission_model(initial_growth_rate,
-                                                           serial_interval,
-                                                           latent_fraction)
-    t_model_c = SEIRModel(**transmission_parameters)
-    e_model_c = EmissionModel(f_cdr, f_cfr,
-                              n_detect, T_detect,
-                              n_resolve, T_resolve,
-                              n_death, T_death)
-    sim_baseline = run_outbreak(t_model_c, e_model_c, 7*sim_time_weeks,
-                                SEIR.make_state(S=6.64e7, I=1))
-    sim_baseline = calibrate_timing_to_cases(sim_baseline,
-                                             observations.confirmed[observations['phase']=='unrestricted'])
-    t_model_ld = SEIRModel(**transmission_parameters)
+
+    unc_model = SEIRObsModel(f_cdr, f_cfr, cv_detect, T_detect, cv_resolve,
+                             T_resolve, cv_death, T_death,
+                             start_date = "2020/02/01",
+                             early_growth_rate = initial_growth_rate,
+                             mean_generation_time = serial_interval,
+                             lat_fraction = latent_fraction,
+                             initial_state = SEIR.make_state(S=6.64e7, I=1))
+    unc_model.fit(observations.confirmed[observations['phase']=='unrestricted'],
+                  observations.recovered[observations['phase']=='unrestricted'],
+                  observations.deaths[observations['phase']=='unrestricted'],
+                  7*sim_time_weeks, weights=weights)
+    sim_baseline = unc_model.predict(7*sim_time_weeks)
+    ld_model = SEIRObsModel(f_cdr, f_cfr, cv_detect, T_detect, cv_resolve,
+                            T_resolve, cv_death, T_death,
+                             start_date = unc_model.start_date,
+                             early_growth_rate = initial_growth_rate,
+                             mean_generation_time = serial_interval,
+                             lat_fraction = latent_fraction,
+                             initial_state = SEIR.make_state(S=6.64e7, I=1))
     R_0_ld = piecewise_linear_R_0_profile(['2020/03/10', '2020/03/26',
                                            lockdown_release_date,
                                            lockdown_release_end],
-                                          [transmission_parameters['R_0'],
+                                          [unc_model.R_0(0),
                                            R_0_lockdown, R_0_lockdown,
-                                           transmission_parameters['R_0']],
+                                           unc_model.R_0(0)],
                                           sim_baseline)
-    t_model_ld.R_0 = R_0_ld
-    sim_ld = run_outbreak(t_model_ld, e_model_c, 7*sim_time_weeks,
-                          SEIR.make_state(S=6.64e7, I=1))
-    sim_ld = calibrate_timing_to_cases(sim_ld,
-                                       observations.confirmed[observations['phase']=='unrestricted'])
+    ld_model.R_0 = R_0_ld
+    sim_ld = ld_model.predict(7*sim_time_weeks)
     assert ((sim_baseline.index - sim_ld.index).to_series().dt.days==0).all()
     _, (axt, axb) = plt.subplots(2, 1, figsize=(12, 16),
                                  gridspec_kw={'height_ratios': [1, 3]})
@@ -129,6 +139,7 @@ def explore_simulation(initial_growth_rate,
 def my_slider(value, mymin, mymax, step, description):
     return FloatSlider(value=value, min=mymin, max=mymax, step=step,
                        description=description,
+                       layout=Layout(width='500px'),
                        style={'description_width': 'initial'})
 
 def my_text_box(value, mymin, mymax, step, description):
@@ -143,19 +154,25 @@ def interactive_simulation(observations):
                        serial_interval = my_slider(6.5, 2, 10, 0.5, 'Mean serial interval, days'),
                        latent_fraction = my_slider(0.71, 0.1, 0.9, 0.1, 'Latent period fraction'),
                        f_cdr = my_slider(4.4, 0.1, 10, 0.1, 'Case detection rate, %'),
-                       f_cfr = my_slider(0.9, 0.1, 5, 0.1, 'Case fatality rate, %'),
+                       f_cfr = my_slider(20, 1, 100, 1, 'Case fatality rate, %'),
                        T_detect = my_slider(11, 1, 30, 1, 'Time to detection, days'),
                        T_resolve = my_slider(8, 1, 30, 1, 'Time to recovery, days'),
-                       T_death = my_slider(10, 1, 30, 1, 'Time to death, days'),
-                       n_detect = my_slider(4, 1, 30, 1, 'Detection time shape'),
-                       n_resolve = my_slider(9, 1, 30, 1, 'Recovery time shape'),
-                       n_death = my_slider(4, 1, 30, 1, 'Death time shape'),
-		       R_0_lockdown = my_slider(0.7, 0.1, 4, 0.1, '$R_0$ during lockdown'),
+                       T_death = my_slider(10, 1, 56, 1, 'Time to death, days'),
+                       cv_detect = my_slider(50, 1, 99, 1, 'Detection time variability, %'),
+                       cv_resolve = my_slider(33, 1, 99, 1, 'Recovery time variability, %'),
+                       cv_death = my_slider(50, 1, 99, 1, 'Death time variability, %'),
+		       R_0_lockdown = my_slider(1.0, 0.1, 4, 0.1, '$R_0$ during lockdown'),
                        lockdown_release_date = Text(value='2020/06/30',
                                                     description='Lockdown release date',
                                                     style={'description_width': 'initial'}),
                        
                        lockdown_release_timeframe_weeks = my_text_box(26, 1, 9999, 1, 'Number of weeks for lockdown release'),
                        sim_time_weeks = my_text_box(52, 1, 999, 1, 'Simulation length, weeks'),
+                       weights = Dropdown(options=[('Cases', [1, 0, 0]),
+                                                   ('Deaths', [0, 0, 1]),
+                                                   ('Cases & deaths',
+                                                    [.5, 0, .5])],
+                                          description='Fit to ',
+                                          style={'description_width': 'initial'}),
                        observations = fixed(observations))
 
